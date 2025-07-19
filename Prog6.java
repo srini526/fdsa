678b516cb41318fe5e0223ede1c7dd6180ada752
srini526 <kangaresrinivas@gmail.com>
2025-07-19 20:13:29 +0530

    Save program before running

---
+++ Prog6.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Prog6 {
+	public static void main(String[] args) {
+	    HashSet<Integer> s=new HashSet<>();
+	    s.add(1);
+	    s.add(2);
+	    s.add(5);
+	    s.add(4);
+	    s.add(4);
+		System.out.println(s);
+		ArrayList<Integer> A=new ArrayList<>();
+	    A.add(1);
+	    A.add(2);
+	    A.add(5);
+	    A.add(4);
+	    A.add(4);
+		System.out.println(A);
+	}
+	
+}
