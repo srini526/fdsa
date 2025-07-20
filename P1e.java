0a0fb57ecad17078e1b6931cead0514558f74848
srini526 <kangaresrinivas@gmail.com>
2025-07-20 11:52:27 +0530

    ughhtgihg

---
+++ P1e.java
@@ -0,0 +1,18 @@
+public class P1e {
+    public static void main(String[] args){
+        System.out.println("Numbers from 1 to 10 are: ");
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+        System.out.println("Numbers from 5 to 15 are: ");
+        for(int i=5;i<=15;i++){
+            System.out.println(i);
+        }
+        System.out.println("Even Numbers from 5 to 14 are: ");
+        for(int i=5;i<=14;i++){
+            if(i%2==0)
+            System.out.println(i);
+        }
+
+    }
+}
