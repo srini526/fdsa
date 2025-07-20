b5f4d38f3242ea1ece6c2fcf5528efe3698bfd47
srini526 <kangaresrinivas@gmail.com>
2025-07-20 09:38:33 +0530

    rsggjb

---
+++ Prog1a.java
@@ -0,0 +1,9 @@
+class Prog1a{
+    public static void main(String[] args) {
+        float pi=3.14f;
+        int r=5;
+        double area=pi*r*r;
+        System.out.println("Area of circle is "+area);
+    }
+
+}
