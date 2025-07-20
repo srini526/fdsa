e98bffc010ddecde6929f4241a4bcda7a8f3cd84
srini526 <kangaresrinivas@gmail.com>
2025-07-20 12:53:18 +0530

    gfvj

---
+++ P1g.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class P1g {
+    public static void main(String[] args) {
+    int c=15;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the no. of candies you want: ");
+    int uc = sc.nextInt();
+    for(int i=0;i<uc;i++){
+        System.out.println("Candy");
+    }
+    if(uc>c){
+        System.out.println("Out of Stock");
+        }
+    }
+
+}
