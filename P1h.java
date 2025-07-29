d573e053db539a000748d4f8d01a375fdee909e5
srini526 <kangaresrinivas@gmail.com>
2025-07-29 22:06:35 +0530

    sjfsygfj

--- P1h.java
+++ P1h.java
@@ -1,5 +1,18 @@
 public class P1h {
     public static void main(String[] args) {
-    
+    for (int i = 0 ; i < 30;  i++){
+        if (i%5==0 && i%3==0) {
+            System.out.println("FillBuzz" + " ");
+        }
+        else if (i%3==0){
+            System.out.println("Fizz"+ " ");
+        }
+    else if (i%5==0){
+        System.out.println("Buzz"+ " ");
+    }
+    else{
+        System.out.println(i+" ");
+    }
+}
 }
 }
