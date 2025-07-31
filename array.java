76add640b428c152a41ab4da76723977630c6bbc
srini526 <kangaresrinivas@gmail.com>
2025-07-31 08:25:49 +0530

    fdgdhj

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String[] args) {
+        int[]a={1,2,3,4,5};
+        for(int i=0;i<5;i++){
+            if(a[i]%2==1)
+            System.out.println(a[i]);
+        }
+    }
+}
